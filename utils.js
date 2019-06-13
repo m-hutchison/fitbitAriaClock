b3679a0c40820f50d7825d915a7b554f94145364
Mathew Hutchison <mathew@hotmail.com.au>
2019-06-14 00:23:24 +0100

    Update utils.js

--- utils.js
+++ utils.js
@@ -1,3 +1,11 @@
+#       This is just a basic clock applet for the FitBit Versa
+#       I'm still learning how this all works, so please be nice
+#                 V1.0 developed by Mathew Hutchison
+#                             13/06/2019
+# 
+#       This file goes in the /common/ folder when designing,
+#       just a basic function for counting the time
+
 // Add zero in front of numbers < 10
 export function zeroPad(i) {
   if (i < 10) {
